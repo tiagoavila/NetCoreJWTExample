214a769bfbbbf26e907aa189a1846b484ff93bee
Tiago Avila <tiago.avila09@gmail.com>
2019-03-09 00:14:00 -0300

    First commit

---
+++ NetCoreJWTExample/Controllers/ConversorAlturasController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreJWTExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConversorAlturasController : ControllerBase
+    {
+        [Authorize("Bearer")]
+        [HttpGet("PesMetros/{alturaPes}")]
+        public object Get(double alturaPes)
+        {
+            return new
+            {
+                AlturaPes = alturaPes,
+                AlturaMetros = Math.Round(alturaPes * 0.3048, 4)
+            };
+        }
+    }
+}
---
+++ NetCoreJWTExample/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreJWTExample.Models
+{
+    public class User
+    {
+        public string UserID { get; set; }
+        public string AccessKey { get; set; }
+    }
+}
